1cdc411782a1fb2d6703d802a3529a766f147409
Story-TellerX <ds.churikov85@gmail.com>
2021-11-05 18:50:32 +0200

    1. Added S3 to resources 2. Added rekognition to resources 3. Function for rekognition and callback is added

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+import json
 import os
 import uuid
 
 import boto3
+from botocore.exceptions import ClientError
 
 from flask import Flask, jsonify, request, make_response
+import requests
 
 app = Flask(__name__)
 
 BLOBS_TABLE = os.environ['BLOBS_TABLE']
 client = boto3.client('dynamodb')
+s3_client = boto3.client('s3')
+bucket = os.environ['BUCKET']
 
 
 @app.route("/")
@@ -28,19 +33,9 @@ def get_blob(blob_id):
     if not item:
         return jsonify({'error': 'Blob not found'}), 404
 
-    labels_data = [
-        {
-            "label": "string",
-            "confidence": 0,
-            "parents": [
-                "string"
-            ]
-        }
-    ]
-
     return jsonify({
         'blob_id': item.get('blob_id').get('S'),
-        'labels': labels_data
+        'labels': item.get('labels')
     })
 
 
@@ -51,23 +46,81 @@ def create_blob():
         return jsonify({'error': 'Invalid callback url supplied'}), 400
 
     blob_id = str(uuid.uuid4())  # Creation of uuid for blob_id
-    BASE_DOMAIN = 'https://0ruf9yg4je.execute-api.us-east-2.amazonaws.com/dev/'
-    upload_url = f'{BASE_DOMAIN} + {blob_id}'
 
     client.put_item(
         TableName=BLOBS_TABLE,
         Item={
             'blob_id': {'S': blob_id},
-            'callback_url': {'S': callback_url},
-            'upload_url': {'S': upload_url}
+            'callback_url': {'S': callback_url}
+            # 'upload_url': {'S': upload_url['url']},
+            # 'labels': {'S': imageLabels}
         }
     )
 
+    # Generate the presigned URL
+    upload_url_raw = s3_client.generate_presigned_post(
+        Bucket=bucket,
+        Key=blob_id,
+        ExpiresIn=3600
+    )
+
+    # Getting data for create a upload url in response
+    url_for_upload = upload_url_raw['url']
+    get_key_data = upload_url_raw['fields']
+    get_key = get_key_data['key']
+    upload_url_full = f'{url_for_upload}{get_key}'
+
     return jsonify({
         'blob_id': blob_id,
-        'callback_url':  callback_url,
-        'upload_url': upload_url
+        'callback_url': callback_url,
+        'upload_url': upload_url_full
     })
+
+
+def rekognition_and_callback(event, context):
+
+    files_uploaded = event['Records']
+    for file in files_uploaded:
+        file_name = file["s3"]["object"]["key"]
+
+        rekognition_client = boto3.client('rekognition')
+        response = rekognition_client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': file_name}},
+                                                    MaxLabels=5, MinConfidence=50)
+
+        image_labels = []
+
+        for label in response['Labels']:
+            image_labels.append(label["Label"].lower())
+            image_labels.append(label["Confidence"].lower())
+            image_labels.append(label["Parents"].lower())
+
+        table = client.Table('BLOBS_TABLE')
+        try:
+            response = table.get_item(Key={'blob_id': file_name})
+            item = response.get('Item')
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            response = table.update_item(
+                Key={
+                    'blob_id': file_name
+                },
+                UpdateExpression="set info.labels=:l",
+                ExpressionAttributeValues={
+                    ':l': list(image_labels)
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+
+            blob_id = item.get('blob_id')
+            callback_url = item.get('callback_url')
+
+            data = {
+                'blob_id': blob_id,
+                'labels': image_labels
+            }
+
+            return requests.post(callback_url, data=json.dumps(data))
 
 
 @app.errorhandler(404)
